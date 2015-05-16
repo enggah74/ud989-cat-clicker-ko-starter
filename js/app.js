9ec806dcc381ed3f2550bb15be3bec8c479a480e
Rosario Safreno <macris936@yahoo.com>
2015-05-16 08:15:49 -0700

    Separate Cat object from ViewModel to include functionality to list all cats and display cat image and click-counter when cat name is clicked

--- js/app.js
+++ js/app.js
@@ -1,31 +1,65 @@
-var Cat = function() {
-	this.clickCount = ko.observable(0);
-	console.dir(this.clickCount());
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+var initialCats = [
+	{ 	"name": "Salt and Pepper Cat",
+		"imgSrc": "img/22252709_010df3379e_z.jpg",
+		"imgAttribution": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVYFdRgNf9rHQdkppGjM7Mg_PwACWxTUdElYkAfaq00jXfe0Anxg",
+		"clickCount": 0,
+		"nicknames": ['Casper']
+	},
+	{	"name": "Cute Cat",
+		"imgSrc": "img/434164568_fea0ad4013_z.jpg",
+		"imgAttribution": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_2Ilm21O2pZdu0AZa7EgVvIu9Jdt-a8NEJVomLkelcpo37Uu2YA",
+		"clickCount": 0,
+		"nicknames": ['Tigger']
+	},
+	{	"name": "Gray Cat",
+		"imgSrc": "img/1413379559_412a540d29_z.jpg",
+		"imgAttribution": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM4mdchZRSr3UmISa886P__z5aOwoIsf8MYKKWnCbFl9u7Stbz",
+		"clickCount": 0,
+		"nicknames": ['Shooby']
+	},
+	{	"name": "Standing Cat",
+		"imgSrc": "img/4154543904_6e2428c421_z.jpg",
+ 		"imgAttribution": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqWa2Jorj4uV90JfiCO5eGbLXaozO8uwpc4fMl95HvLfKDaQKjew",
+		"clickCount": 0,
+		"nicknames": ['Zzzzz']
+	},
+	{	"name": "Lying Cat",
+		"imimgSrc": "img/9648464288_2516b35537_z.jpg",
+		"imgAttribution": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ8Uo2dgcZqUHJXbCwiRDQoIuZNjNcMo-DM0f857MGvK_6RXrQngA",
+		"clickCount": 0,
+		"nicknames": ['Zzzzz']
+	}
+];
 
-	this.names = ko.observableArray([
-        {name: "Tabtab"},
-        {name: "T-Bone"},
-        {name: "Mr. T"},
-        {name: "Tabitha Tab Tabby Catty Cat"},
-    ]);
 
-    this.level = ko.computed(function() {
+
+
+var Cat = function(data) {
+
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observable(data.nicknames);
+
+
+
+    this.title = ko.computed(function() {
+
 		var title;
+		var clicks = this.clickCount();
 
-		if (this.clickCount() < 3) {
+		if (clicks < 4) {
 			title = "Newborn";
-		} else if (this.clickCount() < 13) {
+		} else if (clicks < 13) {
 			title =  "Baby";
-		} else if (this.clickCount() < 25) {
+		} else if (clicks < 25) {
 			title =  "Two-Year-Old";
-		} else if (this.clickCount() < 37) {
+		} else if (clicks < 37) {
 			title =  "Toddler";
-		} else if (this.clickCount() < 240) {
+		} else if (clicks < 240) {
 			title = "Teenager";
-		} else if (this.clickCount() > 240) {
+		} else if (clicks > 240) {
 			title = "Adult";
 		}
 		return title;
@@ -34,10 +68,22 @@ var Cat = function() {
 
 var ViewModel = function() {
 
-	this.currentCat = ko.observable (new Cat() );
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem) {
+		self.catList.push( new Cat(catItem) );
+	});
+
+	this.currentCat = ko.observable ( this.catList() [0] );
 
 	this.incrementCounter = function() {
-		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+
+	this.setCat = function(clickedCat) {
+		self.currentCat(clickedCat);
 	}
 }
 
